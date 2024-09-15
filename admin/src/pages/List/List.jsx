a5933a6de8007b15298496204cebaba6e7cbd6eb
SukarmaGurung <sukarmagurung20@gmail.com>
2024-09-15 08:45:56 +0545

    add item and list item also with toastify

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,32 +1,61 @@
-import React, { useEffect } from 'react'
-import './List.css'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import "./List.css";
+import axios from "axios";
 import { toast } from "react-toastify";
-import { response } from 'express';
 
-const List = () => {
-  const url = 'http://localhost:3000';
-  const [list,setList] = useState([]);
-  const fetchList = async () =>{
-    const response = await axios.get (`${url}/api/product/list`);
-  }
-  if (response.data.success) {
-    setList(response.data.data)
-    
-  } else {
-    toast.error("Error")
-  }
-}
-useEffect(()=>{
-  fetchList();
+const List = ({url}) => {
+ 
+  const [list, setList] = useState([]);
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/product/list`);
+   
 
-},[])
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+  const removeProduct = async(productId) => {
+    const response = await  axios.post(`${url}/api/product/remove`,{id:productId});
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message)
+      
+    } else {
+      toast.error("Error");
+    }
+  };
+  useEffect(() => {
+    fetchList();
+  }, []);
 
   return (
-    <div>
-      
+    <div className="list add flex-col">
+      <p>All product list</p>
+      <div className="list-table">
+        <div className="list-table-format title ">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/` + item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p> Rs{item.price}</p>
+              <p onClick={() => removeProduct(item._id)} className="cursor">
+                X
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-
-
-export default List
+  );
+};
+export default List;
